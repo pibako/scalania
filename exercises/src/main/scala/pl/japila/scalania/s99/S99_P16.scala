39ece5a5c8bc7f71043cb83caf7a2115343a33af
Piotr Kowalski <piotr.kowalski@me.com>
2013-10-14 15:45:46 +0200

    Third solution and last for today.

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P16.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P16.scala
@@ -1,5 +1,16 @@
 package pl.japila.scalania.s99
 
 object S99_P16 {
-  def drop[T](n: Int, ts: Seq[T]): Seq[T] = ???
+  def drop[T](n: Int, ts: Seq[T]): Seq[T] = {
+    def dropNth(n: Int, ts: Seq[T], current: Int): Seq[T] = ts match {
+      case Seq() =>
+        Seq[T]()
+      case x :: xs =>
+        if (current == 3)
+          dropNth(n, xs, 1)
+        else
+          x +: dropNth(n, xs, current + 1)
+    }
+    dropNth(n, ts, 1)
+  }
 }
