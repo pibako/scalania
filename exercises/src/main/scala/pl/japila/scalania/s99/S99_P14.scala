1670a57d21c358b0a0fb8b06619f0ab1ed44970d
Piotr Kowalski <piotr.kowalski@me.com>
2013-10-14 14:51:27 +0200

    Fist exercise done.

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P14.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P14.scala
@@ -1,5 +1,10 @@
 package pl.japila.scalania.s99
 
 object S99_P14 {
-  def duplicate[T](as: Seq[T]): Seq[T] = ???
+  def duplicate[T](as: Seq[T]): Seq[T] = as match {
+    case Seq() =>
+      Seq[T]()
+    case x :: xs =>
+      x +: x +: duplicate(xs)
+  }
 }
