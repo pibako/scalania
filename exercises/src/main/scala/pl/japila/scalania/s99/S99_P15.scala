19cc9e2aad893da6e286c2a605c34e0080a77d8e
Piotr Kowalski <piotr.kowalski@me.com>
2013-10-14 15:31:54 +0200

    Second passed.

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P15.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P15.scala
@@ -1,5 +1,10 @@
 package pl.japila.scalania.s99
 
 object S99_P15 {
-  def duplicateN[T](n: Int, ts: Seq[T]): Seq[T] = ???
+  def duplicateN[T](n: Int, ts: Seq[T]): Seq[T] = ts match {
+    case Seq() =>
+      Seq[T]()
+    case x :: xs =>
+      (0 until n toList).map { _ => x } ++ duplicateN(n, xs)
+  }
 }
